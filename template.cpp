a45d30a081afd551ee2991aaeff6071bccc5edf2
Hector Franco T <42476070+UnPolinomio@users.noreply.github.com>
2022-08-03 21:11:08 -0500

    Add template

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fore(x, a, b) for (lli x = a, ___limit___ = b; x < ___limit___; ++x)
+#define all(x) x.begin(), x.end()
+#define deb(x) cout << #x << (x) << '\n';
+using namespace std;
+using lli = long long int;
+using ii = pair<lli, lli>;
+using vi = vector<lli>;
+using vb = vector<bool>;
+
+int main() {
+  ;
+  return 0;
+}
