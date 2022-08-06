61bffa3205b9880afafaf3b61e4d8ee7d129881b
Hector Franco T <42476070+UnPolinomio@users.noreply.github.com>
2022-08-06 13:32:15 -0500

    Update template

--- template.cpp
+++ template.cpp
@@ -2,6 +2,10 @@
 #define fore(x, a, b) for (lli x = a, ___limit___ = b; x < ___limit___; ++x)
 #define all(x) x.begin(), x.end()
 #define deb(x) cout << #x << (x) << '\n';
+#define _                           \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);                       \
+  cout.tie(0)
 using namespace std;
 using lli = long long int;
 using ii = pair<lli, lli>;
@@ -9,6 +13,6 @@ using vi = vector<lli>;
 using vb = vector<bool>;
 
 int main() {
-  ;
+  _;
   return 0;
 }
